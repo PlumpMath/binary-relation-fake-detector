52cda9ca57623a31e9d125fafe4ae8d7984fd4dc
Anatoliy Stegniy <tsdaemon@gmail.com>
2017-04-19 00:58:14 +0300

    relation extraction for all test articles

--- article_lib.py
+++ article_lib.py
@@ -1,20 +1,31 @@
 import xml.etree.ElementTree as ET
 import pandas as pd
 
+
 def read_article_file_as_datarow(df, path_to_xml):
     e = ET.parse(path_to_xml).getroot()
-    is_revelation = e.findall("is_revelation")[0].text
-    if(is_revelation is None or len(is_revelation) == 0):
-        is_revelation = 0
+    df.loc[len(df)] = [
+        try_int(e.findall("source_id")[0].text),
+        try_int(e.findall("binary_relation_id")[0].text),
+        try_int(e.findall("is_revelation")[0].text),
+        e.findall("title")[0].text,
+        e.findall("url")[0].text,
+        e.findall("body")[0].text]
+
+
+def try_int(s):
+    if s is None or len(s) == 0:
+        return 0
     else:
-        is_revelation = int(is_revelation)
-    df.loc[len(df)] = [int(e.findall("source_id")[0].text), int(e.findall("binary_relation_id")[0].text), is_revelation, e.findall("title")[0].text, e.findall("url")[0].text, e.findall("body")[0].text]
+        return int(s)
+
 
 def read_list_of_articles_as_dataset(ls):
     df = pd.DataFrame(columns=[ "SourceId", "BinaryRelationId", "IsRevelation", "Title", "Url", "Body" ])
     for path in ls:
         read_article_file_as_datarow(df, path)
     return df
+
 
 def join_binary_relations(df):
     path_to_binary_rels = "..\\Data\\fakes.csv"
